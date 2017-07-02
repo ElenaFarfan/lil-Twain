9f2334883727bd1335df748237b6b6b3a47500ec
KenyoJavierFlores <kenyo71816372@hotmail.com>
2017-07-02 15:01:47 -0500

    Buscar por curso y profesor

---
+++ lilTwain-5/js/buscador.js
@@ -0,0 +1,29 @@
+var consulta = $("#searchTable").DataTable();
+
+$("#inputBusqueda").keyup(function(){
+	consulta.search($(this).val()).draw();
+
+	$("header").css({
+		"height": "50vh"
+	
+	})
+
+	if ($("#inputBusqueda").val() == ""){
+		$("header").css({
+			"height": "auto",
+			"background": "none"
+		})
+
+		$("#search").hide()
+
+	} else {
+        $("#search").fadeIn("fast");
+        $("search-table").css({
+            "height": "auto",
+            "background": "none"
+        })
+        
+    }
+   
+
+})
